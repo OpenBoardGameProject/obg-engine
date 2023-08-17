181a5e76fc04d61f796be2c10f9c1695420150b7
Z3ZEL <66113155+Z3ZEL@users.noreply.github.com>
2023-08-18 00:21:33 +0200

    [WIP] - death feature - drop on death

---
+++ src/engine/preconditionners/get_cond.ts
@@ -0,0 +1,20 @@
+import { GameObject } from "../../game_objects/GameInterfaces";
+import { Pawn } from "../../game_objects/Pawn";
+import { Vector2D } from "../math";
+import { GAME_MANAGER } from "../config";
+
+
+
+function CurrentTile(originalMethod : any, context : ClassMethodDecoratorContext){
+    function tmp(this : GameObject, ...args : any[]){
+        // Find the tile where the object is
+        const object_tile = GAME_MANAGER.tiles_manager.tiles_with_objects((obj) => obj == this)
+        if(object_tile.length == 0)
+            throw new Error("Object not found")
+        return originalMethod.call(this, object_tile[0], args)
+    }
+    return tmp;
+}
+
+
+export {CurrentTile}
