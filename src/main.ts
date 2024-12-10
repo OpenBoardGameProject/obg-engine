d9fc72459780e7e78d83c4cb6a6baed4f072e6e8
Z3ZEL <66113155+Z3ZEL@users.noreply.github.com>
2024-12-10 21:08:18 +0100

    [UPDATE DEPS] -uptodate lint

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,10 @@
-
-import board_config from "./config/board_template.json";
 import { BoardConfig } from "./config/config_types";
 import { RESOURCE } from "./engine/config";
-import { Vector2D } from "./engine/math";
-import { ResourceImporter } from "./engine/resource_importer";
-import { GameSerializer } from "./engine/serializer";
 import { GameManager } from "./managers/GameManager";
-import { PrintBoard, PrintPositions } from "./utils/BoardVisualization";
 import { DevConsoleView } from "./utils/ConsoleView";
 
 
 const gameManager = new GameManager(RESOURCE.getRule('dev_rule'), RESOURCE.getConfig('template_board') as BoardConfig);
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const consoleView = new DevConsoleView(gameManager);
 
-
-
-function replacer(key, value){
-    if (key === "object" && value == null){
-        return undefined;
-    }
-    if (key == "adj_matrix_move")
-    {
-        return undefined;
-    }
-    return value;
-}
-
-// console.log(serialized);
